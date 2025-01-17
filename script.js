01e5bcf4ea1a8dc8ad86d95b28edf99684a1f11d
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-01-17 12:47:56 -0500

    create function that gets computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("rps")
+// create a function getComputerChoice
+function getComputerChoice() {
+
+  // initialize a variable "computerChoice" to store computer's choice
+  let computerChoice;
+
+  // generate a random number between 0-2
+  let num = Math.floor(Math.random() * 3)
+
+  // if random number is 0, assign the string "rock" to computerChoice variable
+  if (num === 0) {
+    computerChoice = "rock"
+    // if random number is 1, assign the string "paper" to computerChoice variable
+  } else if (num === 1) {
+    computerChoice = "paper"
+    // if random number is 2, assign the string "scissors" to computerChoice variable
+  } else {
+    computerChoice = "scissors"
+  }
+  // return the variable
+  return computerChoice
+}
