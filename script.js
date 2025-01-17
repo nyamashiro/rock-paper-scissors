e2e0d337fae1724b5cfdfbc7cf741d27c8f5926e
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-01-17 13:37:43 -0500

    write function to play a single round which declares a winner and increments the score appropriately

--- script.js
+++ script.js
@@ -35,3 +35,38 @@ function getHumanChoice() {
   //return the valid input
   return humanChoice
 }
+
+//create function called playRound that takes in two parameters: humanChoice and computerChoice
+function playRound(humanChoice, computerChoice) {
+  //compare the two variables, and determine a winner based on RPS rules
+  if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+    
+    //output a string for e.g. "YOu win/lose! Paper beats Rock"
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+
+    //increment the humanScore or computerScore variable based on the winner of the round
+    humanScore++
+  } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+    
+    //output a string for e.g. "YOu win/lose! Paper beats Rock"
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+
+    //increment the humanScore or computerScore variable based on the winner of the round
+    computerScore++
+  } else {
+    
+    //output a string for e.g. "YOu win/lose! Paper beats Rock"
+    console.log("It's a tie!")
+  }
+}
+
+//program begins to run from here, when we create these variables, they are assigned the return value of the functions. The functions automatically run when the page is loaded
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
+console.log(humanScore, computerScore)
+
+
+
